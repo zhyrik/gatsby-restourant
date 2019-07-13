0c46e4829cd2c679890054c2bf40a0a493657dc3
zhurik <zhyriki@ukr.net>
2019-07-13 23:39:48 +0300

    add multiple quations to galary

--- src/components/HomePage/Galary.js
+++ src/components/HomePage/Galary.js
@@ -22,14 +22,45 @@ function Galary() {
           }
         }
       }
+      img_2: file(relativePath: { eq: "homeGallery/img-2.jpeg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      img_3: file(relativePath: { eq: "homeGallery/img-3.jpeg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
     }
   `)
 
   return (
     <Section>
-      <Img fluid={data.img_1.childImageSharp.fluid} />
+      <Wrapper>
+        <div className="item item1">
+          <Img fluid={data.img_1.childImageSharp.fluid} />
+          <p className="info">awesome piza</p>
+        </div>
+        <div className="item item1">
+          <Img fluid={data.img_2.childImageSharp.fluid} />
+          <p className="info">awesome piza</p>
+        </div>
+        <div className="item item1">
+          <Img fluid={data.img_3.childImageSharp.fluid} />
+          <p className="info">awesome piza</p>
+        </div>
+      </Wrapper>
     </Section>
   )
 }
+
+const Wrapper = styled.div`
+
+`
 
 export default Galary
